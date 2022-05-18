83ce08c7d35df2eaf6bc72ecf805bbf1a836af4f
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-19 00:47:09 +0700

    categories & complate styling cssr

--- src/components/CategoriesBar/CategoriesBar.jsx
+++ src/components/CategoriesBar/CategoriesBar.jsx
@@ -1,7 +1,45 @@
 import React from "react";
-import "./_categoriesBar.scss";
+import "./_categoriesbar.scss";
+
+const keywords = [
+  "All",
+  "React js",
+  "Angular js",
+  "React Native",
+  "use of API",
+  "Redux",
+  "Music",
+  "Algorithm Art ",
+  "Guitar",
+  "Bengali Songs",
+  "Coding",
+  "Cricket",
+  "Football",
+  "Real Madrid",
+  "Gatsby",
+  "Poor Coder",
+  "Shwetabh",
+];
 const CategoriesBar = () => {
-  return <div>CategoriesBar</div>;
+  const [activeElement, setActiveElement] = React.useState("All");
+
+  const handleClick = (value) => {
+    setActiveElement(value);
+  };
+
+  return (
+    <div className="CategoriesBar">
+      {keywords.map((value, i) => (
+        <span
+          onClick={() => handleClick(value)}
+          key={i}
+          className={activeElement === value ? "active" : ""}
+        >
+          {value}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
