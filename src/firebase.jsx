47abbc6e3b3734c88d818052c17674a810b4ce70
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-23 20:51:01 +0700

    settings redux

---
+++ src/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnNRxdq-V50iEse9NDljS2N1PG0FDlRyA",
+  authDomain: "redux-new-clone-yt.firebaseapp.com",
+  projectId: "redux-new-clone-yt",
+  storageBucket: "redux-new-clone-yt.appspot.com",
+  messagingSenderId: "573083812289",
+  appId: "1:573083812289:web:4c9c5226993eef407b6f40",
+};
+
+firebase.initializeApp(firebaseConfig);
