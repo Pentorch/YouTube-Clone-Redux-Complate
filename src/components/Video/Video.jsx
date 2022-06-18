3deec73afc61e5e2bedce145101c2dff8dde96c5
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-06-18 15:35:17 +0700

    Update Responsive TextTitle

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./_video.scss";
-import { AiFillEye } from "react-icons/ai";
+
 import request from "../../api";
 
 import moment from "moment";
