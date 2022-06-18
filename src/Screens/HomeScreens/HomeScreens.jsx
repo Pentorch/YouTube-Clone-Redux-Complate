6cd6be67f61d9c495b3fbde805d301649714598b
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-06-18 17:21:14 +0700

    Infinite Scroll

--- src/Screens/HomeScreens/HomeScreens.jsx
+++ src/Screens/HomeScreens/HomeScreens.jsx
@@ -5,6 +5,8 @@ import CategoriesBar from "../../components/CategoriesBar/CategoriesBar";
 import { getPopularVideos } from "../../redux/actions/videos.action";
 import { useDispatch, useSelector } from "react-redux";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+
 const HomeScreens = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -13,17 +15,30 @@ const HomeScreens = () => {
 
   const { videos } = useSelector((state) => state.homeVideos);
 
+  const fetchData = () => {
+    // LOGIC
+  };
+
   return (
     <div>
       <Container>
         <CategoriesBar />
-        <Row>
+
+        <InfiniteScroll
+          dataLength={videos.length}
+          next={fetchData}
+          hasMore={true}
+          loader={
+            <div className="spinner-border text-danger d-block mx-auto"></div>
+          }
+          className="row"
+        >
           {videos.map((video) => (
             <Col lg={3} md={3} sm={12}>
               <Video video={video} key={video.id} />
             </Col>
           ))}
-        </Row>
+        </InfiniteScroll>
       </Container>
     </div>
   );
