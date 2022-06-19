cde239b0b8ce331a3fcc3d599e79088aa9068b8f
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-06-19 11:23:14 +0700

    Updatel API KEY

--- src/api.js
+++ src/api.js
@@ -3,9 +3,8 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: "AIzaSyBnNRxdq-V50iEse9NDljS2N1PG0FDlRyA",
+    key: "AIzaSyDMKjvW8hjEo9aJaFllG27NWnp6-QmZvGw",
   },
 });
-// console.log(request);
 
 export default request;
