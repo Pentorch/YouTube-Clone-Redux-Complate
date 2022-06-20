5c3fb97de3ce75b966997eb468380adb85b60732
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-06-20 08:08:07 +0700

    Update Redux selectedVideoReducer

--- src/redux/reducers/videos.reducer.jsx
+++ src/redux/reducers/videos.reducer.jsx
@@ -2,6 +2,9 @@ import {
   HOME_VIDEOS_FAIL,
   HOME_VIDEOS_REQUEST,
   HOME_VIDEOS_SUCCESS,
+  SELECTED_VIDEO_FAIL,
+  SELECTED_VIDEO_REQUEST,
+  SELECTED_VIDEO_SUCCESS,
 } from "../actionType";
 
 export const homeVideosReducer = (
@@ -37,6 +40,39 @@ export const homeVideosReducer = (
       return {
         ...state,
         loading: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export const selectedVideoReducer = (
+  state = {
+    loading: true,
+    video: null,
+  },
+  action
+) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SELECTED_VIDEO_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SELECTED_VIDEO_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        video: payload,
+      };
+    case SELECTED_VIDEO_FAIL:
+      return {
+        ...state,
+        loading: false,
+        video: null,
+        error: payload,
       };
     default:
       return state;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -5,10 +5,12 @@ import thunk from "redux-thunk";
 
 import { authReducer } from "./reducers/auth.reducer";
 import { homeVideosReducer } from "./reducers/videos.reducer";
+import { selectedVideoReducer } from "./reducers/videos.reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   homeVideos: homeVideosReducer,
+  selectedVideo: selectedVideoReducer,
 });
 
 const store = createStore(
