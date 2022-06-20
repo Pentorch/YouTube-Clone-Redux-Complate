c89965d82f79e663af02a6db50e580612fefe0b3
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-06-20 08:19:48 +0700

    Update Redux VideoRequest

--- src/Screens/WatchScreen/WatchScreen.jsx
+++ src/Screens/WatchScreen/WatchScreen.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import Comments from "../../components/Comments/Comments";
 import VideoHorizontal from "../../components/VideoHorizontal/VideoHorizontal";
 import VideoMetaData from "../../components/VideoMetaData/VideoMetaData";
+import { getVideoById } from "../../redux/actions/videos.action";
 import "./_watchScreen.scss";
 const WatchScreen = () => {
   const { id } = useParams();
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getVideoById(id));
+  }, [dispatch, id]);
+
   return (
     <Row>
       <Col lg={8}>
